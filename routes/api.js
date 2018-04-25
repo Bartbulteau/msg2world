c496bc8d3abe86475acd05d29a5b943aeb7667a3
Bartbulteau <barthelemybulteau@gmail.com>
2018-04-25 02:40:10 +0200

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* Fake Database */
+var JsonDB = require('node-json-db');
+// The second argument is used to tell the DB to save after each push
+// If you put false, you'll have to call the save() method.
+// The third argument is to ask JsonDB to save the database in an human readable format. (default false)
+var db = new JsonDB("database", true, true);
+
+/* Default GET */
+router.get('/', function(req,res, next) {
+  res.send('Please go to /api/messages to get messages');
+});
+
+/* GET all messages. */
+router.get('/messages', function(req, res, next) {
+  res.json(db.getData('/messages'));
+});
+
+/* POST one message */
+router.post('/message', function(req, res, next) {
+  try {
+    db.push('/messages[]', req.body);
+    res.json(db.getData('/messages'));
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+/* DELETE message with id */
+router.delete('/message/:id', function(req, res, next) {
+  try {
+    db.delete('/messages[' + req.params.id + ']');
+    res.json(db.getData('/messages'));
+  } catch (e) {
+    console.error(e);
+  }
+});
+  
+
+module.exports = router;
