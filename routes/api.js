5c0d19dd2764cd23434d122294cbc73515e82c37
Bartbulteau <barthelemybulteau@gmail.com>
2018-04-26 00:28:20 +0200

    Fix socket io and live msg removing

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* SET UP DATABASE */
+// set-up database
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://bart:20BarT01@ds255329.mlab.com:55329/m2w');
 var db = mongoose.connection;
 
 // check connection
 db.once('open', function () {
-    console.log('Connected to MongoDB Messages');
+    console.log('Connected to MongoDB');
 });
 
 db.on('error', function (err) {
     console.log(err);
 });
 
-// bring in models
+// bring in model
 var Message = require('../database/models/message');
-
-
-
-
-
-
-
 
 
 /* Default GET */
